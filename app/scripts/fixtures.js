154483777208796a0bbe9ce0e5c2bcdd1f1cc432
twr349 <twr349@gmail.com>
2017-08-17 10:29:20 -0700

    Checkpoint 5 Controllers Completed

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,46 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+      { title: 'Blue', duration: '161.71', audioUrl: 'assets/music/blue' },
+      { title: 'Green', duration: '103.96', audioUrl: 'assets/music/green' },
+      { title: 'Red', duration: '268.45', audioUrl: 'assets/music/red' },
+      { title: 'Pink', duration: '153.14', audioUrl: 'assets/music/pink' },
+      { title: 'Magenta', duration: '374.22', audioUrl: 'assets/music/magenta' }
+    ]
+};
+
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumDjango = {
+    title: 'Djangology',
+    artist: 'Django Reinhardt',
+    label: 'Not Now',
+    year: '1949',
+    albumArtUrl: 'assets/images/album_covers/django.jpg',
+    songs: [
+        { title: 'Minor Swing', duration: '2:37' },
+        { title: 'Beyond the sea', duration: '4:24' },
+        { title: 'Bricktop', duration: '3:45' },
+        { title: 'Honeysuckle rose', duration: '4:00' },
+        { title: 'Heavy artillery', duration: '3.41' },
+        { title: 'Djangology', duration: '2:19' }
+    ]
+
+};
