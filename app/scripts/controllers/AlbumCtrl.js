925c2149cc3cea4574dd75661a746424f3a14c24
twr349 <twr349@gmail.com>
2017-08-17 15:18:03 -0700

    Assignment 5 Controllers Completed

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,13 @@
+(function () {
+  function AlbumCtrl() {
+
+    this.albumData = angular.copy(albumPicasso);
+    
+
+}
+
+  angular
+    .module('blocJams')
+    .controller('AlbumCtrl', AlbumCtrl);
+
+})();
